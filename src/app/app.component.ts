5c892a5936ac389d7c2937bae610eeab7a3000c2
sharath <sharath.s@adwitiya.io>
2018-10-30 21:43:15 +0530

    Master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
+import {AngularFireDatabase} from 'angularfire2/database';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'firebase-demo';
+  fruits$;
+
+  constructor(db: AngularFireDatabase) {
+    this.fruits$ = firebase.database().ref('/fruits').once('value').then(snapshot => {
+      return snapshot;
+    });
+    /*
+        firebase.database().ref('/fruits').once('value').then(snapshot => {
+          this.fruits = snapshot.val();
+        });
+    */
+  }
 }
